e7b82f3001bda76be3f9d1833705b2aa51113175
sk814 <sk814@student.le.ac.uk>
2020-12-26 13:56:21 +0000

    Edit page

--- src/app/edit-product-form/edit-product-form.component.ts
+++ src/app/edit-product-form/edit-product-form.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-product-form',
@@ -10,8 +11,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class EditProductFormComponent implements OnInit {
 
   editProductForm; 
+  productId:any;
 
-  constructor(private fb: FormBuilder,private http: HttpClient) { 
+  constructor(private router : Router,private fb: FormBuilder,private http: HttpClient,private route: ActivatedRoute) {
 
     this.editProductForm=this.fb.group({
       product_id:[null,Validators.required],
@@ -24,7 +26,23 @@ export class EditProductFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+
+    this.productId = this.route.snapshot.paramMap.get('id');
+
+
+    this.http.get("http://localhost:8000/api/products/"+this.productId+"/").subscribe(product=>{
+   const productObb:any = product;
+   
+    this.editProductForm.controls.product_id.setValue(productObb.product_id);
+    this.editProductForm.controls.product_name.setValue(productObb.product_name);
+    this.editProductForm.controls.stock.setValue(productObb.stock);
+    this.editProductForm.controls.selling_price.setValue(productObb.selling_price);
+    this.editProductForm.controls.cost_price.setValue(productObb.cost_price);
+
+
+
+
+    })
   }
 
  
@@ -34,11 +52,11 @@ export class EditProductFormComponent implements OnInit {
    const formValue = this.editProductForm.value;
     this.http
       .put(
-        'http://localhost:8000/api/products/'+this.editProductForm.controls.product_id.value,
+        'http://localhost:8000/api/products/'+this.editProductForm.controls.product_id.value+"/",
         formValue
       )
       .subscribe(responseData => {
-        console.log(responseData);
+      this.router.navigate(["/products"])
       });
   }
 
